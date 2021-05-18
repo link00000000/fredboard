1a98e1528c47f11ee26a3367d906349191b4f89b
Logan Crandall <crandall.logan@gmail.com>
2021-05-18 19:27:53 -0400

    Set metadata FileDescription to 'FredBoard'

--- fredboard/Arguments.py
+++ fredboard/Arguments.py
@@ -2,7 +2,7 @@ import argparse
 
 from .Metadata import metadata
 
-_parser = argparse.ArgumentParser(prog=metadata.ProductName, description=metadata.FileDescription)
+_parser = argparse.ArgumentParser(prog=metadata.ProductName, description="Global hotkeys for Discord music bots")
 _parser.add_argument('--color', '-c', dest="enable_colored_output", action="store_true")
 _parser.add_argument('--version', '-v', action="version", version=metadata.ProductName + ' v' + metadata.Version)
 
