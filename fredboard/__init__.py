0d96a96903e83d2979dd5acde304115651ad36c3
= <=>
2024-10-31 22:06:28 -0400

    Clear repo

--- fredboard/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-All subclasses of AbstractMusicBot must be imported
-here to be referenced reflexively. They do not need to
-be imported elsewhere from this file.
-"""
-from .MusicBots.FredBoat import *
-from .MusicBots.Rythm import *
