a1f51e85cc73a906244f565c734effb331c2bcea
= <=>
2024-11-01 00:08:39 -0400

    Add /yt command that receives url

--- main.go
+++ main.go
@@ -29,23 +29,26 @@ func main() {
     logger.Debug("Set log level", "level", level.String())
   }
 
-  _, ok := os.LookupEnv("DISCORD_APP_ID")
+  discordAppId, ok := os.LookupEnv("DISCORD_APP_ID")
   if !ok {
     logger.Error("Required environment variable not set: DISCORD_APP_ID")
     os.Exit(1)
   }
+  logger.Debug("Read environment variable DISCORD_APP_ID", "value", discordAppId)
 
-  _, ok = os.LookupEnv("DISCORD_PUBLIC_KEY")
+  discordPublicKey, ok := os.LookupEnv("DISCORD_PUBLIC_KEY")
   if !ok {
     logger.Error("Required environment variable not set: DISCORD_PUBLIC_KEY")
     os.Exit(1)
   }
+  logger.Debug("Read environment variable DISCORD_PUBLIC_KEY", "value", discordPublicKey)
 
   discordToken, ok := os.LookupEnv("DISCORD_TOKEN")
   if !ok {
     logger.Error("Required environment variable not set: DISCORD_TOKEN")
     os.Exit(1)
   }
+  logger.Debug("Read environment variable DISCORD_TOKEN", "value", "[secret]")
 
   session, err := discordgo.New("Bot " + discordToken)
   if err != nil {
@@ -53,7 +56,34 @@ func main() {
     os.Exit(1)
   }
 
+  logger.Debug("Registering handlers")
   session.AddHandler(onReady)
+  session.AddHandler(onInteractionCreate)
+
+  logger.Debug("Registering commands")
+  newCmds, err := session.ApplicationCommandBulkOverwrite(discordAppId, "", []*discordgo.ApplicationCommand{
+    &discordgo.ApplicationCommand{
+      Type: discordgo.ChatApplicationCommand,
+      Name: "yt",
+      Description: "Play a YouTube video",
+      Options: []*discordgo.ApplicationCommandOption{
+        &discordgo.ApplicationCommandOption{
+          Type: discordgo.ApplicationCommandOptionString,
+          Name: "url",
+          Description: "Url to the YouTube video to play",
+          Required: true,
+        },
+      },
+    },
+  })
+  if err != nil {
+    logger.Error("Failed to register commands", "error", err)
+    os.Exit(1)
+  }
+
+  for _, cmd := range newCmds {
+    logger.Debug("Registered new command", "name", cmd.Name, "type", cmd.Type)
+  }
 
   err = session.Open()
   if err != nil {
@@ -76,4 +106,61 @@ func main() {
 
 func onReady(s *discordgo.Session, e *discordgo.Ready) {
   logger.Info("Session opened", "event", e)
+}
+
+func onInteractionCreate(s *discordgo.Session, e *discordgo.InteractionCreate) {
+  logger.Debug("InteractionCreate event received", "guildId", e.GuildID, "channelId", e.ChannelID)
+
+  switch data := e.Data.(type) {
+  case discordgo.ApplicationCommandInteractionData:
+    if data.Name == "yt" {
+      logger.Debug("Interaction matched type /yt", "event", e)
+
+      var url string
+      for _, opt := range data.Options {
+        switch opt.Name {
+        case "url":
+          if opt.Type != discordgo.ApplicationCommandOptionString {
+            logger.Error("Option received does not match the registered type for interaction /yt",
+              "name", "url",
+              "registeredType", discordgo.ApplicationCommandOptionString,
+              "receivedOption", opt,
+            )
+            continue
+          }
+
+          url = opt.StringValue()
+          logger.Debug("Received option for /yt", "name", "url", "value", opt.StringValue())
+
+        default:
+          logger.Warn("Received unknown option for interaction /yt", "option", opt)
+        }
+      }
+
+      if len(url) == 0 {
+        logger.Error("Did not receive required option for interaction /yt", "name", "url", "event", e)
+        return
+      }
+
+      res := &discordgo.InteractionResponse{
+        Type: discordgo.InteractionResponseChannelMessageWithSource,
+        Data: &discordgo.InteractionResponseData{
+          Content: "YouTube video will now play...",
+        },
+      }
+
+      err := s.InteractionRespond(e.Interaction, res)
+      if err != nil {
+        logger.Error("Failed to respond to interaction /yt", "event", e, "error", err)
+        return
+      }
+
+      logger.Debug("Responded to interaction /yt", "event", e, "response", res)
+      return
+    }
+
+    logger.Warn("Command interaction unknown", "event", e)
+  default:
+    logger.Warn("Interaction type not supported", "event", e)
+  }
 }
