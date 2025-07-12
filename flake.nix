9770e612f26d48d8efba2a471f7359cf422091a2
= <=>
2025-07-11 22:34:50 -0400

    Add docker-compose with monitoring

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs { inherit system; };
     
     buildInputs = with pkgs; [
@@ -40,44 +40,55 @@
       hardeningDisable = [ "fortify" ];
     };
 
-    packages.default = pkgs.buildGo123Module {
-      pname = "fredboard";
-      version = builtins.readFile ./version;
-      src = ./.;
+    packages = {
+      default = self.packages.${system}.fredboard-server;
 
-      subPackages = [
-        "cmd/fredboard_server"
-      ];
+      fredboard-server = pkgs.buildGo123Module {
+        pname = "fredboard";
+        version = builtins.readFile ./version;
+        src = ./.;
 
-      vendorHash = "sha256-urCKBLWE4Pjiy1q0hSxEzu8XK1OR2VMkNyHSnai3EkM=";
+        subPackages = [
+          "cmd/fredboard_server"
+        ];
 
-      # gopus package has C files that are not properly vendored by 'go mod vendor',
-      # so we need to manually add them to the vendored directory.
-      preBuild = let
-        gopusRepo = let
-          goMod = builtins.readFile ./go.mod;
-          matches = builtins.match ".*layeh.com/gopus v[0-9.]+-[0-9]+-([a-f0-9]+).*" goMod;
-          rev = if matches != null && matches != [] then (builtins.head matches) else throw "gopus rev not found";
-        in pkgs.fetchFromGitHub {
-          inherit rev;
-          owner = "layeh";
-          repo = "gopus";
-          sha256 = "sha256-i1N5ETtqTfmLZ3yb4yhZXJsGBsykxFyifdSo94Th8RU=";
+        vendorHash = "sha256-urCKBLWE4Pjiy1q0hSxEzu8XK1OR2VMkNyHSnai3EkM=";
+
+        # gopus package has C files that are not properly vendored by 'go mod vendor',
+        # so we need to manually add them to the vendored directory.
+        preBuild = let
+          gopusRepo = let
+            goMod = builtins.readFile ./go.mod;
+            matches = builtins.match ".*layeh.com/gopus v[0-9.]+-[0-9]+-([a-f0-9]+).*" goMod;
+            rev = if matches != null && matches != [] then (builtins.head matches) else throw "gopus rev not found";
+          in pkgs.fetchFromGitHub {
+            inherit rev;
+            owner = "layeh";
+            repo = "gopus";
+            sha256 = "sha256-i1N5ETtqTfmLZ3yb4yhZXJsGBsykxFyifdSo94Th8RU=";
+          };
+        in /* bash */ ''
+          # vendor/ is not writable, so we need to make a copy of everything to a writable version of vendor/
+          mv vendor vendor.old
+          mkdir -p vendor/layeh.com/gopus/opus-1.1.2
+          cp -r ${gopusRepo}/opus-1.1.2/* vendor/layeh.com/gopus/opus-1.1.2/
+          cp -r vendor.old/* vendor/
+        '';
+
+        meta = {
+          description = "A music player bot for Discord";
+          homepage = "https://github.com/link00000000/fredboard";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ link00000000 ];
         };
-      in /* bash */ ''
-        # vendor/ is not writable, so we need to make a copy of everything to a writable version of vendor/
-        mv vendor vendor.old
-        mkdir -p vendor/layeh.com/gopus/opus-1.1.2
-        cp -r ${gopusRepo}/opus-1.1.2/* vendor/layeh.com/gopus/opus-1.1.2/
-        cp -r vendor.old/* vendor/
-      '';
-
-      meta = {
-        description = "A music player bot for Discord";
-        homepage = "https://github.com/link00000000/fredboard";
-        license = pkgs.lib.licenses.mit;
-        maintainers = with pkgs.lib.maintainers; [ link00000000 ];
       };
+
+      monitoring = pkgs.writeShellScriptBin "start-monitoring" ''
+        set -e
+        cd $(mktemp -d)
+        cp ${./docker-compose.yml} docker-compose.yml
+        exec ${pkgs.docker-compose}/bin/docker-compose up
+      '';
     };
   });
 }
