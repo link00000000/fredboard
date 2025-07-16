45feb12c9df32d390cfd2e24f19f33271a3a84b7
link00000000 <crandall.logan@gmail.com>
2025-07-15 21:27:51 -0400

    Setup loki, prometheus, and tempo

--- nix/nixos-configurations/fredboard-monitoring.nix
+++ nix/nixos-configurations/fredboard-monitoring.nix
@@ -1,5 +1,14 @@
 { config, ... }:
 
+let
+  grafanaPort = config.services.grafana.settings.server.http_port;
+  #lokiPort = config.services.loki.configuration.server.http_listen_port;
+  lokiPort = 3100;
+  #tempoPort = config.services.tempo.settings.server.http_listen_port;
+  tempoPort = 3200;
+  prometheusPort = config.services.prometheus.port;
+in
+
 {
   system.stateVersion = "25.05";
 
@@ -10,17 +19,24 @@
     virtualisation.cores = 4;
     virtualisation.memorySize = 2 * 1024;
     virtualisation.diskSize = 32 * 1024;
-    #virtualisation.diskImage = "./.qemu/${config.system.name}.qcow2";
-    virtualisation.forwardPorts = [
-      { host.port = 3000; guest.port = 3000; }
-      { host.port = 3100; guest.port = 3100; }
-      { host.port = 3200; guest.port = 3200; }
-      { host.port = 9009; guest.port = 9009; }
-    ];
+    virtualisation.diskImage = "./.qemu/${config.system.name}.qcow2";
+    virtualisation.forwardPorts = let
+      forward = port: { host.port = port; guest.port = port; };
+    in [
+      (forward grafanaPort)
+      (forward lokiPort)
+      (forward tempoPort)
+      (forward prometheusPort)
+    ] ++ (builtins.map forward config.services.openssh.ports);
   };
 
-  users.users.root.password = "";
+  networking.firewall.enable = false;
+  users.users.root.password = "root";
   services.getty.autologinUser = "root";
+  services.openssh = {
+    enable = true;
+    ports = [ 2222 ];
+  };
 
   services.grafana = {
     enable = true;
@@ -36,102 +52,27 @@
           name = "Loki";
           type = "loki";
           access = "proxy";
-          url = "http://localhost:3100";
+          url = "http://localhost:${builtins.toString lokiPort}";
         }
         {
           name = "Tempo";
           type = "tempo";
           access = "proxy";
-          url = "http://localhost:3200";
+          url = "http://localhost:${builtins.toString tempoPort}";
         }
         {
           name = "Prometheus";
           type = "prometheus";
           access = "proxy";
-          url = "http://localhost:9009";
+          url = "http://localhost:${builtins.toString prometheusPort}";
         }
       ];
     };
   };
 
-/*
   services.loki = {
     enable = true;
-    configuration = {
-      auth_enabled = false;
-      server.http_listen_port = 3100;
-      ingester = {
-        lifecycler = {
-          ring = {
-            kvstore.store = "inmemory";
-          };
-          final_sleep = "0s";
-        };
-        chunk_idle_period = "5m";
-        max_chunk_age = "1h";
-      };
-      schema_config = {
-        configs = [{
-          from = "2022-01-01";
-          store = "boltdb-shipper";
-          object_store = "filesystem";
-          schema = "v11";
-          index = {
-            prefix = "index_";
-            period = "24h";
-          };
-        }];
-      };
-      storage_config = {
-        boltdb_shipper = {
-          active_index_directory = "/tmp/loki/index";
-          cache_location = "/tmp/loki/cache";
-        };
-        filesystem.directory = "/tmp/loki/chunks";
-      };
-    };
-  };
-  */
-
-  services.promtail = {
-    enable = true;
-    configuration = {
-      server.http_listen_port = 9080;
-      positions = {
-        filename = "/tmp/positions.yaml";
-      };
-      clients = [
-        { url = "http://localhost:3100/loki/api/v1/push"; }
-      ];
-      scrape_configs = [
-        {
-          job_name = "system";
-          static_configs = [{
-            targets = ["localhost"];
-            labels = {
-              job = "syslog";
-              __path__ = "/var/log/*.log";
-            };
-          }];
-        }
-      ];
-    };
-  };
-
-  services.tempo = {
-    enable = true;
-    settings = {
-      server.http_listen_port = 3200;
-      distributor = { receivers = { otlp = { protocols.grpc.endpoint = "localhost:4317"; }; }; };
-      ingester = { };
-      compactor = { };
-      storage = {
-        trace = {
-          backend = "local";
-          local = { path = "/tmp/tempo/traces"; };
-        };
-      };
-    };
+    configFile = ./loki/config.yaml;
   };
 
   services.prometheus = {
@@ -141,17 +82,22 @@
     scrapeConfigs = [
       {
         job_name = "loki";
-        static_configs = [{ targets = [ "localhost:3100" ]; }];
+        static_configs = [{ targets = [ "localhost:${builtins.toString lokiPort}" ]; }];
       }
       {
         job_name = "tempo";
-        static_configs = [{ targets = [ "localhost:3200" ]; }];
+        static_configs = [{ targets = [ "localhost:${builtins.toString tempoPort}" ]; }];
       }
       {
         job_name = "grafana";
-        static_configs = [{ targets = [ "localhost:3000" ]; }];
+        static_configs = [{ targets = [ "localhost:${builtins.toString grafanaPort}" ]; }];
       }
     ];
+  };
+
+  services.tempo = {
+    enable = true;
+    configFile = ./tempo/config.yaml;
   };
 }
 
