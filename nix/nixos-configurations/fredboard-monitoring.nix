a2c936acc3c8d8d4e51f53e731a41698f3dbe4d4
link00000000 <crandall.logan@gmail.com>
2025-07-22 20:07:43 -0400

    Add opentelemetry-collector service

--- nix/nixos-configurations/fredboard-monitoring.nix
+++ nix/nixos-configurations/fredboard-monitoring.nix
@@ -24,11 +24,17 @@ in
       (forward lokiCfg.configuration.server.http_listen_port)
       (forward tempoCfg.settings.server.http_listen_port)
       (forward prometheusCfg.port)
+      (forward 4317)
+      (forward 4318)
     ];
   };
 
   networking.firewall.enable = false;
   users.users.root.password = "";
+  users.motd = builtins.concatStringsSep "\n" [
+    "Grafana: http://localhost:${builtins.toString grafanaCfg.settings.server.http_port}"
+    "QEMU Help: Ctrl-b h"
+  ];
   services.getty.autologinUser = "root";
 
   services.grafana = {
@@ -83,6 +89,38 @@ in
         static_configs = [{ targets = [ "localhost:${builtins.toString grafanaCfg.settings.server.http_port}" ]; }];
       }
     ];
+  };
+
+  services.tempo = {
+    enable = true;
+    settings = {
+      server = {
+        http_listen_port = 3200;
+        grpc_listen_port = 9096;
+      };
+
+      distributor = {
+        receivers.otlp.protocols = {
+          grpc = {};
+          http = {};
+        };
+      };
+
+      ingester = {
+        trace_idle_period = "10s";
+        max_block_duration = "5m";
+      };
+
+      compactor = {
+        compaction.block_retention = "1h";
+      };
+
+      storage.trace = {
+        backend = "local";
+        local.path = "/var/lib/tempo/data/tempo/blocks";
+        wal.path = "/var/lib/tempo/data/tempo/wal";
+      };
+    };
   };
 
   services.loki = {
@@ -157,34 +195,165 @@ in
     };
   };
 
-  services.tempo = {
+  services.opentelemetry-collector = {
     enable = true;
-    settings = {
-      server = {
-        http_listen_port = 3200;
-        grpc_listen_port = 9096;
-      };
 
-      distributor = {
-        receivers.otlp.protocols = {
-          grpc = {};
-          http = {};
+    # The structure of any Collector configuration file consists of four classes of pipeline components that access telemetry data:
+    #
+    # * Receivers
+    # * Processors
+    # * Exporters
+    # * Connectors 
+    #
+    # After each pipeline component is configured you must enable it using the pipelines within the service section of the configuration file.
+    #
+    # Besides pipeline components you can also configure extensions, which provide capabilities that can be added to the Collector,
+    # such as diagnostic tools. Extensions don’t require direct access to telemetry data and are enabled through the service section.
+    #
+    # docs: https://opentelemetry.io/docs/collector/configuration/#basics
+    settings = {
+      # Receivers collect telemetry from one or more sources. They can be pull or push based, and may support one or more data sources.
+      # Receivers are configured in the receivers section. Many receivers come with default settings, so that specifying the name of the
+      # receiver is enough to configure it. If you need to configure a receiver or want to change the default configuration, you can do
+      # so in this section. Any setting you specify overrides the default values, if present.
+      #
+      # Configuring a receiver does not enable it. Receivers are enabled by adding them to the appropriate pipelines within the service section.
+      #
+      # The Collector requires one or more receivers.
+      #
+      # docs: https://opentelemetry.io/docs/collector/configuration/#receivers
+      receivers = {
+        otlp = {
+          protocols = {
+            grpc.endpoint = "0.0.0.0:4317";
+            http.endpoint = "0.0.0.0:4318";
+          };
         };
       };
 
-      ingester = {
-        trace_idle_period = "10s";
-        max_block_duration = "5m";
+      # Processors take the data collected by receivers and modify or transform it before sending it to the exporters. Data processing
+      # happens according to rules or settings defined for each processor, which might include filtering, dropping, renaming, or
+      # recalculating telemetry, among other operations. The order of the processors in a pipeline determines the order of the processing
+      # operations that the Collector applies to the signal.
+      #
+      # Processors are optional, although some are recommended.
+      #
+      # You can configure processors using the processors section of the Collector configuration file. Any setting you specify overrides
+      # the default values, if present.
+      #
+      # Configuring a processor does not enable it. Processors are enabled by adding them to the appropriate pipelines within the
+      # service section.
+      #
+      # docs: https://opentelemetry.io/docs/collector/configuration/#processors
+      processors = {
+        memory_limiter = {
+          check_interval = "5s";
+          limit_mib = 4000;
+          spike_limit_mib = 500;
+        };
       };
 
-      compactor = {
-        compaction.block_retention = "1h";
+      # Exporters send data to one or more backends or destinations. Exporters can be pull or push based, and may support one or
+      # more data sources.
+      #
+      # Each key within the exporters section defines an exporter instance, The key follows the type/name format, where type specifies
+      # the exporter type (e.g., otlp, kafka, prometheus), and name (optional) can be appended to provide a unique name for multiple
+      # instance of the same type.
+      #
+      # Most exporters require configuration to specify at least the destination, as well as security settings, like authentication
+      # tokens or TLS certificates. Any setting you specify overrides the default values, if present.
+      #
+      # Configuring an exporter does not enable it. Exporters are enabled by adding them to the appropriate pipelines within the service section.
+      #
+      # The Collector requires one or more exporters.
+      #
+      # docs: https://opentelemetry.io/docs/collector/configuration/#exporters
+      exporters = {
+        prometheusremotewrite.endpoint = "http://localhost:${builtins.toString prometheusCfg.port}/api/v1/write";
+        "otlp/tempo".endpoint = "localhost:${builtins.toString tempoCfg.settings.server.http_listen_port}";
+        "otlp/loki".endpoint = "http://localhost:${builtins.toString lokiCfg.configuration.server.http_listen_port}";
       };
 
-      storage.trace = {
-        backend = "local";
-        local.path = "/var/lib/tempo/data/tempo/blocks";
-        wal.path = "/var/lib/tempo/data/tempo/wal";
+      # Connectors join two pipelines, acting as both exporter and receiver. A connector consumes data as an exporter at the end of one
+      # pipeline and emits data as a receiver at the beginning of another pipeline. The data consumed and emitted may be of the same
+      # type or of different data types. You can use connectors to summarize consumed data, replicate it, or route it.
+      #
+      # You can configure one or more connectors using the connectors section of the Collector configuration file. By default,
+      # no connectors are configured. Each type of connector is designed to work with one or more pairs of data types and may only
+      # be used to connect pipelines accordingly.
+      #
+      # Configuring a connector doesn’t enable it. Connectors are enabled through pipelines within the service section.
+      #
+      # docs: https://opentelemetry.io/docs/collector/configuration/#connectors
+      connectors = {};
+
+      # Extensions are optional components that expand the capabilities of the Collector to accomplish tasks not directly involved
+      # with processing telemetry data. For example, you can add extensions for Collector health monitoring, service discovery, or
+      # data forwarding, among others.
+      #
+      # You can configure extensions through the extensions section of the Collector configuration file. Most extensions come with
+      # default settings, so you can configure them just by specifying the name of the extension. Any setting you specify
+      # overrides the default values, if present.
+      #
+      # Configuring an extension doesn’t enable it. Extensions are enabled within the service section.
+      #
+      # By default, no extensions are configured.
+      #
+      # docs: https://opentelemetry.io/docs/collector/configuration/#extensions
+      extensions = {};
+
+      # The service section is used to configure what components are enabled in the Collector based on the configuration found in
+      # the receivers, processors, exporters, and extensions sections. If a component is configured, but not defined within the
+      # service section, then it’s not enabled.
+      #
+      # docs: https://opentelemetry.io/docs/collector/configuration/#service
+      service = {
+        # The extensions subsection consists of a list of desired extensions to be enabled.
+        #
+        # docs: https://opentelemetry.io/docs/collector/configuration/#service-extensions
+        extensions = [];
+
+        # The pipelines subsection is where the pipelines are configured, which can be of the following types:
+        #
+        # * `traces` collect and processes trace data.
+        # * `metrics` collect and processes metric data.
+        # * `logs` collect and processes log data.
+        #
+        # A pipeline consists of a set of receivers, processors and exporters. Before including a receiver, processor, or exporter
+        # in a pipeline, make sure to define its configuration in the appropriate section.
+        #
+        # You can use the same receiver, processor, or exporter in more than one pipeline. When a processor is referenced in
+        # multiple pipelines, each pipeline gets a separate instance of the processor.
+        #
+        # Note that the order of processors dictates the order in which data is processed.
+        #
+        # As with components, use the type[/name] syntax to create additional pipelines for a given type. Here is an example
+        # extending the previous configuration.
+        #
+        # docs: https://opentelemetry.io/docs/collector/configuration/#pipelines
+        pipelines = {
+          metrics = {
+            receivers = [ "otlp" ];
+            processors = [ "memory_limiter" ];
+            exporters = [ "prometheusremotewrite" ];
+          };
+          traces = {
+            receivers = [ "otlp" ];
+            processors = [ "memory_limiter" ];
+            exporters = [ "otlp/tempo" ];
+          };
+          logs = {
+            receivers = [ "otlp" ];
+            processors = [ "memory_limiter" ];
+            exporters = [ "otlp/loki" ];
+          };
+        };
+
+        # The telemetry config section is where you can set up observability for the Collector itself.
+        # It consists of two subsections: logs and metrics.
+        #
+        # docs: https://opentelemetry.io/docs/collector/configuration/#telemetry
+        telemetry = {};
       };
     };
   };
