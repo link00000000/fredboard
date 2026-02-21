87ddfe4fd0f4469bd7e3f38546d81280703dedfb
Logan Crandall <crandall.logan@gmail.com>
2026-02-21 10:19:01 -0500

    Add D++ example

--- Fretboard/Fretboard.cpp
+++ Fretboard/Fretboard.cpp
@@ -1,9 +1,28 @@
 #include "Fretboard.h"
 
+#include <dpp/dpp.h>
+
 namespace Fretboard
 {
     int Main()
     {
+    	constexpr auto DiscordBotToken = "";
+	    dpp::cluster DiscordBot(DiscordBotToken);
+
+	    DiscordBot.on_log(dpp::utility::cout_logger());
+
+	    DiscordBot.on_slashcommand([](const dpp::slashcommand_t& event) {
+	        if (event.command.get_command_name() == "ping") {
+	            event.reply("Pong!");
+	        }
+	    });
+
+	    DiscordBot.on_ready([&DiscordBot](const dpp::ready_t& _event) {
+			DiscordBot.global_command_create(dpp::slashcommand("ping", "Ping pong!", DiscordBot.me.id));
+	    });
+
+	    DiscordBot.start(dpp::st_wait);
+
         return 0;
     }
 }
