15ec25806ac9ef02c795b7f030210f80ca8d6869
Logan Crandall <crandall.logan@gmail.com>
2026-03-14 19:09:32 -0400

    Remove completed TODOs

--- DeveloperConsole/DeveloperConsole/ControlServer.cpp
+++ DeveloperConsole/DeveloperConsole/ControlServer.cpp
@@ -102,12 +102,6 @@ namespace DeveloperConsole
         {
             Core::Log::Error("ControlServer", "Failed to execute unknown command {}", Command);
         }
-
-        // TODO
-        // 1. Get the command from the string
-        // 2. Get the arguments from the string
-        // 3. Execute registered command if there is one
-        // 4. Output error message if there is no registered command
     }
 
     bool ControlServer::SendData(const std::span<std::byte> Data)
