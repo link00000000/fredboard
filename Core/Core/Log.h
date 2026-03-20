91240a2554313b5d8187ad77095ec11a39c85c3f
Logan Crandall <crandall.logan@gmail.com>
2026-03-20 20:40:23 +0000

    Add missing includes

--- Core/Core/Log.h
+++ Core/Core/Log.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <format>
 #include <functional>
 #include <string_view>
 
--- DeveloperConsole/DeveloperConsole/ControlServer.h
+++ DeveloperConsole/DeveloperConsole/ControlServer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include "Transports/ITransport.h"
 
 namespace DeveloperConsole
